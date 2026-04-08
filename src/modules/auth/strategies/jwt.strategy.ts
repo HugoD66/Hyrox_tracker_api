58eb4d3425b58e70a330144991e7b99e05b34db3
Maurane H <maurane.hugon@gmail.com>
2026-04-08 10:48:55 +0200

    fix(auth): expose userId in jwt user

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -15,9 +15,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   public async validate(payload: {
     sub: string;
     email: string;
-  }): Promise<{ id: string; email: string }> {
+  }): Promise<{ id: string; userId: string; email: string }> {
     return {
       id: payload.sub,
+      userId: payload.sub,
       email: payload.email,
     };
   }
